1c625249595cb2a9b22269bddd5ac0c82adb82a7
Tarun2811 <tarunguduru123@gmail.com>
2026-05-16 15:52:18 +0530

    Added selection sort

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,30 @@
+package Sorting;
+
+public class SelectionSort {
+    
+    public static void selectionSort(int[] arr){
+        int n=arr.length;
+
+        for(int i=0;i<n-1;i++){
+            int minIndex = i;
+            for(int j=i+1;j<n;j++){
+                if(arr[j] < arr[minIndex]){
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {6,5,1,3};
+        selectionSort(arr);
+
+        for(int val:arr){
+            System.out.print(val + " ");
+        }
+    
+    }
+}
