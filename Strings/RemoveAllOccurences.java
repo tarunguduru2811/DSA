5c5e1ddc8134fd49d04fe2218a4bd4a6e2f4a9a8
Tarun2811 <tarunguduru123@gmail.com>
2026-06-17 14:24:59 +0530

    Solved Remove all occurences of substring question

---
+++ Strings/RemoveAllOccurences.java
@@ -0,0 +1,20 @@
+package Strings;
+
+public class RemoveAllOccurences {
+    public static String removeAllOccurencesofPart(String s,String part){
+        int n = s.length();
+        int m = part.length();
+
+        while(s.contains(part)){
+            int firstIndexofPart = s.indexOf(part);
+            s = s.substring(0,firstIndexofPart) + s.substring(firstIndexofPart+m,s.length());    
+        }
+
+        return s;
+    }
+    public static void main(String[] args) {
+        String s = "axxxxyyyyb";
+        String part = "xy";
+        System.out.println(removeAllOccurencesofPart(s, part));
+    }
+}
